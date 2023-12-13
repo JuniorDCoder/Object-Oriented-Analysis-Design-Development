b9eccb7720fe1bdeaeefa9dd9cdf58e8aa5a497a
JuniorDCoder <foryoungjuniorngu@gmail.com>
2023-12-13 01:56:25 +0100

    feat: created implementation file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main(){
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Hello World!" << endl;
+    
+    string name;
+    cout<<"Please enter your name: "<<endl;
+    cin>>name;
+
+    cout<<"Hello "<<name<<"! Welcome to my world"<<endl;
+    
+    return 0;
+}
